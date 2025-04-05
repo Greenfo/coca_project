3dac5548c02ab50396ad1b5b9bef7fae93dbc4d1
atay <adambaevatay@gmail.com>
2025-04-05 23:56:18 +0700

    Created testimonials setction

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 import '../assets/scss/main.scss';
+import { Navigation } from 'swiper/modules';
 
 // components
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
-import { useInsightSlider } from './components/slider';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/home/slider';
 
 useTheme();
 useBurger();
 useInsightSlider();
+useTestimonialsSlider();
