5c235dc77e6d47aaf8ff225a5b4e604fc0c6b174
atay <adambaevatay@gmail.com>
2025-04-09 19:43:23 +0700

    Created contacts page

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       input: {
         main: './src/index.html',
         pricing: './src/pricing.html',
+        contact: './src/contact.html',
       },
     },
   },
